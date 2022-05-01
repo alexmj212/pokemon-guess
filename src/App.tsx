6716cf3055a9e469e912db65144a8187a17e4131
Alex Johnson <alexmj212@gmail.com>
2022-05-01 17:02:15 -0400

    who's that pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,106 @@
 import "./App.css";
+import PokeAPI, {
+  INamedApiResource,
+  IPokemon,
+  IPokemonSpecies,
+} from "pokeapi-typescript";
+import { useEffect, useState } from "react";
+import PokemonDescription from "./PokemonDescription";
 
-function App() {
+const App = () => {
+  const [pokemonList, setPokemonList] = useState<
+    INamedApiResource<IPokemonSpecies>[]
+  >([]);
+  const [randomPokemon, setRandomPokemon] =
+    useState<INamedApiResource<IPokemonSpecies>>();
+  const [randomId, setRandomId] = useState<number>(0);
+
+  const [revealedPokemon, setRevealedPokemon] = useState<IPokemon>();
+
+  const [showPokemon, setShowPokemon] = useState<boolean>(false);
+
+  const generateRandomNumber = (max: number) => {
+    return Math.floor(Math.random() * max);
+  };
+
+  useEffect(() => {
+    PokeAPI.PokemonSpecies.listAll(true).then((pokemon) => {
+      setPokemonList(pokemon.results);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (pokemonList.length > 0) {
+      setRandomId(generateRandomNumber(pokemonList.length));
+    }
+  }, [pokemonList]);
+
+  useEffect(() => {
+    if (randomId && pokemonList.length > 0) {
+      setRandomPokemon(pokemonList[randomId]);
+    }
+  }, [pokemonList, randomId]);
+
+  const reset = () => {
+    setRandomId(generateRandomNumber(pokemonList.length));
+    setShowPokemon(false);
+    setRevealedPokemon({} as IPokemon);
+  };
+
+  const revealPokemon = (name: string) => {
+    setShowPokemon(true);
+    PokeAPI.Pokemon.resolve(name).then((pokemon) => {
+      setRevealedPokemon(pokemon);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-full">
+      <div className="flex flex-col justify-center w-full h-full max-w-4xl mx-auto py-2 px-2 sm:px-4">
+        <h1 className="text-center text-4xl font-bold py-8">{`Who's that Pokémon?`}</h1>
+        {randomPokemon && (
+          <>
+            <PokemonDescription
+              key={randomPokemon.name}
+              pokemon={randomPokemon}
+            />
+
+            <div className="text-center h-[200px] flex flex-col justify-center items-center">
+              {showPokemon &&
+              revealedPokemon &&
+              revealedPokemon.sprites &&
+              revealedPokemon.sprites.front_default ? (
+                <>
+                  <div className="text-4xl font-bold capitalize">
+                    {revealedPokemon.name}
+                  </div>
+                  <img
+                    src={revealedPokemon.sprites.front_default}
+                    alt={revealedPokemon.name}
+                  />
+                </>
+              ) : (
+                <div className="text-4xl font-bold text-slate-500">???</div>
+              )}
+            </div>
+            <div className="flex justify-center">
+              {" "}
+              <button
+                disabled={showPokemon}
+                className="button-positive"
+                onClick={() => revealPokemon(randomPokemon?.name)}
+              >
+                Reveal Pokemon
+              </button>
+              <button className="button-outline" onClick={reset}>
+                Try Another
+              </button>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/PokemonDescription.tsx
@@ -0,0 +1,45 @@
+import PokeAPI, {
+  INamedApiResource,
+  IPokemon,
+  IPokemonSpecies,
+} from "pokeapi-typescript";
+import React, { useEffect, useState } from "react";
+
+type PokemonDescriptionProps = {
+  pokemon: INamedApiResource<IPokemonSpecies>;
+};
+
+const PokemonDescription: React.FC<PokemonDescriptionProps> = ({ pokemon }) => {
+  const [description, setDescription] = useState<string>();
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const pokemonSpecies = await PokeAPI.PokemonSpecies.resolve(pokemon.name);
+      const nameRegEx = new RegExp(pokemonSpecies.name, "ig");
+      const flavorEntries = pokemonSpecies.flavor_text_entries
+        .filter((entry) => entry.language.name === "en")
+        .filter(
+          ({ flavor_text }, index) =>
+            !flavor_text.includes(flavor_text, index + 1)
+        )
+        .map((entry) => {
+          return {
+            ...entry,
+            flavor_text: entry.flavor_text.replace(nameRegEx, "it"),
+          };
+        });
+      const randomFlavorText = flavorEntries[
+        Math.floor(Math.random() * flavorEntries.length)
+      ].flavor_text.replace("", "");
+      setDescription(randomFlavorText);
+    };
+    fetchPokemon();
+  }, []);
+
+  return (
+    <div className="min-h-[20px] flex items-center justify-center">
+      {description && <span className="text-2xl text-center capitalize">{description}</span>}
+    </div>
+  );
+};
+
+export default PokemonDescription;
