7f1a47b5b0194a20d1c47315d326443ef4724d0b
Alex Johnson <alexmj212@gmail.com>
2022-05-02 00:12:22 -0400

    Add type display

--- src/PokemonDescription.tsx
+++ src/PokemonDescription.tsx
@@ -1,7 +1,4 @@
-import {
-  IPokemon,
-  IPokemonSpecies,
-} from "pokeapi-typescript";
+import { IPokemon, IPokemonSpecies } from "pokeapi-typescript";
 import React, { useEffect, useState } from "react";
 
 type PokemonDescriptionProps = {
@@ -9,31 +6,50 @@ type PokemonDescriptionProps = {
   pokemonSpecies: IPokemonSpecies;
 };
 
-const PokemonDescription: React.FC<PokemonDescriptionProps> = ({ pokemonSpecies }) => {
+const PokemonDescription: React.FC<PokemonDescriptionProps> = ({
+  pokemon,
+  pokemonSpecies,
+}) => {
   const [description, setDescription] = useState<string>();
   useEffect(() => {
-      const nameRegEx = new RegExp(pokemonSpecies.name, "ig");
-      const flavorEntries = pokemonSpecies.flavor_text_entries
-        .filter((entry) => entry.language.name === "en")
-        .filter(
-          ({ flavor_text }, index) =>
-            !flavor_text.includes(flavor_text, index + 1)
-        )
-        .map((entry) => {
-          return {
-            ...entry,
-            flavor_text: entry.flavor_text.replace(nameRegEx, "It"),
-          };
-        });
-      const randomFlavorText = flavorEntries[
-        Math.floor(Math.random() * flavorEntries.length)
-      ].flavor_text.replace("", " ");
-      setDescription(randomFlavorText);
+    const nameRegEx = new RegExp(pokemonSpecies.name, "ig");
+    const flavorEntries = pokemonSpecies.flavor_text_entries
+      .filter((entry) => entry.language.name === "en")
+      .filter(
+        ({ flavor_text }, index) =>
+          !flavor_text.includes(flavor_text, index + 1)
+      )
+      .map((entry) => {
+        return {
+          ...entry,
+          flavor_text: entry.flavor_text.replace(nameRegEx, "<Pokémon Name>"),
+        };
+      });
+    const randomFlavorText = flavorEntries[
+      Math.floor(Math.random() * flavorEntries.length)
+    ].flavor_text.replace("", " ");
+    setDescription(randomFlavorText);
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <div className="min-h-24 flex items-center justify-center">
-      {description && <span className="text-2xl text-center">{description}</span>}
+    <div className="min-h-[150px] flex items-center justify-center">
+      {description && (
+        <div className="flex flex-col">
+          <span className="pokedex-description">{description}</span>
+          <div className="flex flex-row justify-center py-4 gap-4">
+            {pokemon?.types.map((type, index) => {
+              return (
+                <span
+                  key={`${type.type}-${index}`}
+                  className={`type-badge ${type.type.name}`}
+                >
+                  {type.type.name}
+                </span>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
