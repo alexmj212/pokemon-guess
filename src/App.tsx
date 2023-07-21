216c8bc16085cb4cffdaef7793357f67f2c06a03
Alex Johnson <alexmj212@gmail.com>
2023-07-21 01:10:28 -0400

    styling, implement assets, prettier run

--- src/App.tsx
+++ src/App.tsx
@@ -37,26 +37,37 @@ const App = () => {
 
   const getRandomGenerationSpecies = (filterList: number[]) => {
     const randomGen = generateRandomNumber(filterList.length);
-    PokeAPI.game.getGenerationById(randomGen).then((generation) => {
-      getRandomSpecies(generation.pokemon_species);
-    });
+    PokeAPI.game
+      .getGenerationById(randomGen)
+      .then((generation) => {
+        getRandomSpecies(generation.pokemon_species);
+      })
+      .catch((e) => {
+        console.error("Unable to get Generation", randomGen, e);
+      });
   };
 
   const getRandomSpecies = (speciesList: NamedAPIResource[]) => {
-    const randomSpecies = generateRandomNumber(speciesList.length);
+    const randomSpecies = speciesList[generateRandomNumber(speciesList.length)];
+    if (randomSpecies && "name" in randomSpecies) {
     PokeAPI.pokemon
-      .getPokemonSpeciesByName(speciesList[randomSpecies].name)
+      .getPokemonSpeciesByName(randomSpecies.name)
       .then((species) => {
         setPokemonSpecies(species);
+        return species;
       })
-      .then(() => {
-        PokeAPI.pokemon
-          .getPokemonByName(speciesList[randomSpecies].name)
-          .then((pokemon) => {
-            setPokemon(pokemon);
-          });
+      .then((species) => {
+        PokeAPI.pokemon.getPokemonById(species.id).then((pokemon) => {
+          setPokemon(pokemon);
+        });
       })
-      .finally(() => setTimeout(() => setLoading(false), 1000));
+      .finally(() => setTimeout(() => setLoading(false), 1000))
+      .catch((e) => {
+        console.error("Unable to get random species", randomSpecies, e);
+      });
+    } else {
+      throw "Unable to get random pokemon";
+    }
   };
 
   const initializeGame = () => {
@@ -90,71 +101,90 @@ const App = () => {
   const reset = () => {
     setLoading(true);
     getRandomGenerationSpecies(generationFilters);
+    setPokemon({} as any);
+    setPokemonSpecies({} as any);
     setShowPokemon(false);
   };
 
   return (
     <div className="w-full h-full">
-      <div className="flex flex-col w-full h-full max-w-xl mx-auto py-2 px-2 sm:px-4">
-        <div>
+      <div className="flex flex-col w-full h-full max-w-4xl mx-auto py-2 px-2 sm:px-4">
+        <div className="main-frame">
           <h1 className="page-title">{`Who's that PoKéMoN?`}</h1>
-          {loading && <>Loading...</>}
+          {loading && (
+            <div className="flex w-full h-[500px] justify-center items-center m-auto pokedex-description align-center">
+              {" "}
+              <div className="poke_box">
+                <div className="pokeball" onClick={() => setShowPokemon(true)}>
+                  <div className="pokeball__button"></div>
+                </div>
+              </div>
+            </div>
+          )}
           {!loading && pokemon && pokemonSpecies && (
-            <>
+            <div className="flex flex-col-reverse sm:flex-row h-[500px]">
+              <div className="pokemon-box">
+                <div className="text-center min-h-[250px] md:min-h-[450px] flex flex-col justify-center items-center">
+                  {
+                  pokemon &&
+                  pokemon.sprites &&
+                  pokemon.sprites.front_default ? (
+                    <>
+                      <div className="pokemon-name text-4xl font-bold capitalize">
+                        {
+                          showPokemon ? pokemonSpecies.names.filter(
+                            (entry) => entry.language.name === "en"
+                          )[0].name : '???'
+                        }
+                      </div>
+                      <img
+                        className={`pokemon-sprite ${showPokemon ? '' : 'hidden-pokemon'}`}
+                        src={
+                          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                          // @ts-ignore
+                          pokemon.sprites["other"]["official-artwork"][
+                            "front_default"
+                          ] ?? pokemon.sprites.front_default
+                        }
+                        alt={pokemon.name}
+                      />
+                    </>
+                  ) : (
+                    <div className="poke_box">
+                      <div
+                        className="pokeball"
+                        onClick={() => setShowPokemon(true)}
+                      >
+                        <div className="pokeball__button"></div>
+                      </div>
+                    </div>
+                  )}
+                </div>
+                <div className="flex flex-col items-center">
+                  {showPokemon ? (
+                    <button className="button-outline" onClick={reset}>
+                      Try Another
+                    </button>
+                  ) : (
+                    <button
+                      className="button-positive"
+                      onClick={() => {
+                        const audio = new Audio('./chime.mp3');
+                        audio.play();
+                        setShowPokemon(true)
+                      }}
+                    >
+                      Reveal Pokemon
+                    </button>
+                  )}
+                </div>
+              </div>
               <PokemonDescription
                 key={pokemon.name}
                 pokemon={pokemon}
                 pokemonSpecies={pokemonSpecies}
               />
-
-              <div className="text-center min-h-[250px] md:min-h-[450px] flex flex-col justify-center items-center">
-                {showPokemon &&
-                pokemon &&
-                pokemon.sprites &&
-                pokemon.sprites.front_default ? (
-                  <>
-                    <div className="pokemon-name text-4xl font-bold capitalize">
-                      {
-                        pokemonSpecies.names.filter(
-                          (entry) => entry.language.name === "en"
-                        )[0].name
-                      }
-                    </div>
-                    <img
-                      className="pokemon-sprite"
-                      src={
-                        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                        // @ts-ignore
-                        pokemon.sprites["other"]["official-artwork"][
-                          "front_default"
-                        ] ?? pokemon.sprites.front_default
-                      }
-                      alt={pokemon.name}
-                    />
-                  </>
-                ) : (
-                    <div className="poke_box">
-                      <div className="pokeball" onClick={() => setShowPokemon(true)}>
-                        <div className="pokeball__button"></div>
-                      </div>
-                    </div>
-                )}
-              </div>
-              <div className="flex flex-col items-center">
-                {showPokemon ? (
-                  <button className="button-outline" onClick={reset}>
-                    Try Another
-                  </button>
-                ) : (
-                  <button
-                    className="button-positive"
-                    onClick={() => setShowPokemon(true)}
-                  >
-                    Reveal Pokemon
-                  </button>
-                )}
-              </div>
-            </>
+            </div>
           )}
         </div>
         <div className="flex flex-initial flex-wrap flex-row justify-between border-t-2 border-slate-300 dark:border-slate-500 mt-4 py-4 text-xs">
--- src/PokemonDescription.tsx
+++ src/PokemonDescription.tsx
@@ -13,22 +13,26 @@ const PokemonDescription: React.FC<PokemonDescriptionProps> = ({
   const [description, setDescription] = useState<string>();
   useEffect(() => {
     const nameRegEx = new RegExp(pokemonSpecies.name, "ig");
-    const flavorEntries = pokemonSpecies.flavor_text_entries
-      .filter((entry) => entry.language.name === "en")
-      .filter(
-        ({ flavor_text }, index) =>
-          !flavor_text.includes(flavor_text, index + 1)
-      )
-      .map((entry) => {
-        return {
-          ...entry,
-          flavor_text: entry.flavor_text.replace(nameRegEx, "<Pokémon Name>"),
-        };
-      });
-    const randomFlavorText = flavorEntries[
-      Math.floor(Math.random() * flavorEntries.length)
-    ].flavor_text.replace("", " ");
-    setDescription(randomFlavorText);
+    try {
+      const flavorEntries = pokemonSpecies.flavor_text_entries
+        .filter((entry) => entry.language.name === "en")
+        .filter(
+          ({ flavor_text }, index) =>
+            !flavor_text.includes(flavor_text, index + 1)
+        )
+        .map((entry) => {
+          return {
+            ...entry,
+            flavor_text: entry.flavor_text.replace(nameRegEx, "<Pokémon Name>"),
+          };
+        });
+      const randomFlavorText = flavorEntries[
+        Math.floor(Math.random() * flavorEntries.length)
+      ].flavor_text.replace("", " ");
+      setDescription(randomFlavorText);
+    } catch {
+      setDescription("There isn't enough information about this Pokémon.");
+    }
   }, [pokemon, pokemonSpecies]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
@@ -36,8 +40,11 @@ const PokemonDescription: React.FC<PokemonDescriptionProps> = ({
       {description && (
         <div className="flex flex-col">
           <span className="pokedex-description">{description}</span>
-          <div className="pokedex-description flex flex-row justify-center py-4 gap-4 uppercase">
-            <div>Gen: {pokemonSpecies?.generation?.name.split("-")[1]}</div>
+          <div className="pokedex-description flex flex-row justify-center py-4 gap-4">
+            First Appearance:{" "}
+            <div className="uppercase">
+              Gen {pokemonSpecies?.generation?.name.split("-")[1]}
+            </div>
           </div>
           <div className="flex flex-row justify-center py-4 gap-4">
             {pokemon?.types.map((type, index) => {
